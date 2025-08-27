c75a6c235cb041aae3fa45c4761f8622af09c7e6
wvxbs <gabriel.ferreira7854@gmail.com>
2025-08-27 17:43:24 -0300

    ex3 pronto

--- src/ex3_contador.c
+++ src/ex3_contador.c
@@ -40,20 +40,24 @@ int main() {
      * TODO 1: Implementar loop de leitura
      * Loop até read() retornar 0 (fim do arquivo)
      */
-    while (/* TODO: condição do loop */) {
+    while ((bytes_lidos = read(fd, buffer, BUFFER_SIZE)) > 0) {
         total_reads++;
         
         /*
          * TODO 2: Contar caracteres '\n' no buffer
          */
         for (int i = 0; i < bytes_lidos; i++) {
-            /* TODO: verificar '\n' e incrementar total_linhas */
+            if (buffer[i] == '\n') {
+                total_linhas++;
+            }
         }
         
         /*
          * TODO 3: Somar total de caracteres
          */
         /* TODO: total_caracteres += ... */;
+
+        total_caracteres += bytes_lidos;
         
         if (total_reads % 10 == 0) {
             printf("Processadas %d chamadas read()...\n", total_reads);
@@ -63,7 +67,7 @@ int main() {
     /*
      * TODO 4: Verificar se houve erro na leitura
      */
-    if (/* TODO: condição de erro */) {
+    if (bytes_lidos < 0) {
         perror("Erro na leitura");
         close(fd);
         return 1;
