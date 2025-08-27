f2717588340586f32dacd797ea9ee45695e6d297
wvxbs <gabriel.ferreira7854@gmail.com>
2025-08-27 17:41:06 -0300

    ex1 e ex2 prontos

--- src/ex2_leitura.c
+++ src/ex2_leitura.c
@@ -27,13 +27,13 @@ int main() {
      * TODO 1: Abrir o arquivo 'dados/teste1.txt' para leitura
      * Use open() com O_RDONLY
      */
-    fd = /* COMPLETE AQUI */;
+    fd = open("dados/teste1.txt", O_RDONLY);
     
     /*
      * TODO 2: Verificar se a abertura foi bem-sucedida
      * Se fd < 0, houve erro
      */
-    if (/* COMPLETE AQUI */) {
+    if (fd < 0) {
         perror("Erro ao abrir arquivo");
         return 1;
     }
@@ -44,12 +44,13 @@ int main() {
      * TODO 3: Ler dados do arquivo
      * Use read() para ler até (BUFFER_SIZE - 1) bytes
      */
-    bytes_lidos = /* COMPLETE AQUI */;
+    bytes_lidos = read(fd, buffer, BUFFER_SIZE - 1);
     
     /*
      * TODO 4: Verificar se a leitura foi bem-sucedida
      */
-    if (/* COMPLETE AQUI */) {
+
+    if (bytes_lidos < 0) {
         perror("Erro na leitura");
         close(fd);
         return 1;
@@ -60,7 +61,7 @@ int main() {
      * Para tratar o buffer como string
      */
     /* COMPLETE AQUI */;
-    
+    buffer[bytes_lidos] = '\0';
     // Exibir resultados
     printf("Bytes lidos: %ld\n", bytes_lidos);
     printf("Conteúdo:\n%s\n", buffer);
@@ -69,7 +70,7 @@ int main() {
      * TODO 6: Fechar o arquivo
      * Use close() para liberar o file descriptor
      */
-    if (/* COMPLETE AQUI */) {
+    if (close(fd) < 0) {
         perror("Erro ao fechar arquivo");
         return 1;
     }
